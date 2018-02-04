3d86045575bfc9e0ca868122a1c343ace89e6de1
Toril Rygg <torilrygg@gmail.com>
2018-02-04 10:57:47 +0100

    GUI is now single window with popup-windows for map and setup

--- GaiaSetup.py
+++ GaiaSetup.py
@@ -4,7 +4,6 @@ import random
 from Gaia import Map
 
 demo_map_path = "images/MapDemo.png"
-standard_map_path = "images/StandardMap.png"
 default_map_path = "images/Gaia_map.png"
 background_path = "images/Tech_bg.png"
 image_path = "images/"
@@ -19,8 +18,7 @@ FIN = ['FINbld', 'FINfed', 'FINgai', 'FINsec', 'FINsat', 'FINtyp']
 FED = ['FEDknw', 'FEDore', 'FEDcre', 'FEDqic', 'FEDpwt']
 
 list_of_pieces = [FED[:], ADV[:], TEC[:], TEC[:], BOO[:], RND[:], FIN[:]]
-num_pics = [1, 6, 6, 3, 7, 6, 2]
-color_list = [(160, 101, 85),(110, 131, 162),(88, 162, 51),(171, 11, 119),(247, 165, 1),(0, 156, 223),(159, 165, 176)]
+
 
 class MainFrame(wx.Frame):
     def __init__(self, parent=None):
@@ -31,19 +29,45 @@ class MainFrame(wx.Frame):
         self.SetFont(self.default_font)
 
         self.default_num_players = 2
+        self.SetBackgroundColour(wx.WHITE)
 
         #icon
 
-        self.panel = wx.Panel(self)
-        self.notebook = NotebookStructure(self, self.panel)
-        self.map_tab = self.notebook.get_map_tab()
-        self.setup_tab = self.notebook.get_setup_tab()
-
         vsizer = wx.BoxSizer(wx.VERTICAL)
-        vsizer.Add(self.notebook, 1, wx.ALL | wx.EXPAND, 5)
-        self.panel.SetSizer(vsizer)
+
+        hsizer_main = wx.BoxSizer(wx.HORIZONTAL)
+        vsizer_player_info = wx.BoxSizer(wx.VERTICAL)
+
+        players_info_text = wx.StaticText(self, -1, "Number of players")
+        self.players_number = wx.TextCtrl(self, value=str(self.default_num_players))
+        btn_make_map = wx.Button(self, wx.ID_ADD, label="Generate map", size=(120, 40))
+        self.Bind(wx.EVT_BUTTON, self.on_make_map, btn_make_map)
+        btn_randomize = wx.Button(self, wx.ID_PAGE_SETUP, label="Randomize setup", size=(140, 40))
+        self.Bind(wx.EVT_BUTTON, self.on_randomize, btn_randomize)
+
+        vsizer_player_info.Add(players_info_text, 1)
+        vsizer_player_info.Add(self.players_number, 1)
+
+        hsizer_main.Add(vsizer_player_info, 1, wx.EXPAND | wx.ALL, 20)
+        hsizer_main.Add(btn_make_map, 1, wx.EXPAND | wx.ALL, 20)
+        hsizer_main.Add(btn_randomize, 1, wx.EXPAND | wx.ALL, 20)
+
+        vsizer.Add(hsizer_main, 0, wx.EXPAND)
+
+        self.SetSizer(vsizer)
         self.Centre()
         self.Show()
+
+    def on_randomize(self, event):
+        n_players = int(self.players_number.GetValue())
+        random_setup = RandomSetup(self, n_players)
+        random_setup.Show(True)
+
+    def on_make_map(self, event):
+        n_players = int(self.players_number.GetValue())
+
+        map = MapGenerator(self, n_players)
+        map.Show(True)
 
     def make_menu(self):
         pass
@@ -54,140 +78,52 @@ class MainFrame(wx.Frame):
     def get_default_num_players(self):
         return self.default_num_players
 
+class MapGenerator(wx.Frame):
+    def __init__(self, parent, num_players, image_path=default_map_path):
+        super(MapGenerator, self).__init__(parent, title="Map Generator", size=(1050, 850))
+        num_players = num_players
 
-class NotebookStructure(wx.Notebook):
-    def __init__(self, parent, panel):
-        wx.Notebook.__init__(self, panel, id=wx.ID_ANY, style=wx.BK_DEFAULT)
-        self.parent = parent
-        self.panel = panel
-        self.map_tab = MapTab(self)
-        self.setup_tab = SetupTab(self)
+        PhotoMaxHeight = 800
+        self.SetBackgroundColour(wx.WHITE)
 
-        self.AddPage(self.map_tab, "Map")
-        self.AddPage(self.setup_tab, "Setup")
+        map = Map(num_players)
+        map.save_image_map()
 
-    def get_map_tab(self):
-        return self.map_tab
-
-    def get_setup_tab(self):
-        return self.setup_tab
-
-    def get_default_num_players(self):
-        return self.parent.get_default_num_players()
-
-class MapTab(wx.Panel):
-    def __init__(self, parent):
-        super(MapTab, self).__init__(parent=parent, id=wx.ID_ANY)
-        self.parent = parent
-        self.png = None
-        self.default_num_players = self.parent.get_default_num_players()
-        self.PhotoMaxSize = 800
-
-        img = wx.Image(standard_map_path, wx.BITMAP_TYPE_ANY)
-        W = img.GetWidth()
-        H = img.GetHeight()
-        if W > H:
-            NewW = self.PhotoMaxSize
-            NewH = self.PhotoMaxSize * H / W
-        else:
-            NewH = self.PhotoMaxSize
-            NewW = self.PhotoMaxSize * W / H
-
-        img = img.Scale(NewW, NewH)
-        self.imageMap = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap(img))
-
-        self.hsizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.hsizer2 = wx.BoxSizer(wx.HORIZONTAL)
-        self.vsizer = wx.BoxSizer(wx.VERTICAL)
-        self.vsizer2 = wx.BoxSizer(wx.VERTICAL)
-
-        players_info_text = wx.StaticText(self, -1, "Number of players")
-        self.players_number = wx.TextCtrl(self, value=str(self.default_num_players))
-        btn_make_map = wx.Button(self, wx.ID_OK, label="Generate map", size=(120, 40))
-        self.Bind(wx.EVT_BUTTON, self.on_make_map, btn_make_map)
-
-        self.vsizer.Add(players_info_text, 1)
-        self.vsizer.Add(self.players_number, 1)
-
-        self.hsizer.Add(self.vsizer, 1, wx.EXPAND | wx.ALL, 20)
-        self.hsizer.Add(btn_make_map, 1, wx.EXPAND | wx.ALL, 20)
-        self.hsizer2.Add(self.imageMap, 0, wx.ALL, 5)
-        self.vsizer2.Add(self.hsizer, 0)    # setup
-        self.vsizer2.Add(self.hsizer2, 0, wx.EXPAND)
-
-        self.SetSizer(self.vsizer2)
-
-    def on_make_map(self, event):
-        n_players = int(self.players_number.GetValue())
-        self.map = Map(n_players)
-
-        # TODO: optimize!!!!!
-
-        self.map.save_image_map()
-        self.set_map(default_map_path)
-
-    def set_map(self, image_path):
         img = wx.Image(image_path, wx.BITMAP_TYPE_ANY)
         W = img.GetWidth()
         H = img.GetHeight()
-        if W > H:
-            NewW = self.PhotoMaxSize
-            NewH = self.PhotoMaxSize * H / W
-        else:
-            NewH = self.PhotoMaxSize
-            NewW = self.PhotoMaxSize * W / H
+        NewH = PhotoMaxHeight
+        NewW = PhotoMaxHeight * W / H
+
         img = img.Scale(NewW, NewH)
-        self.imageMap.SetBitmap(wx.Bitmap(img))
+        imageMap = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap(img))
 
+        hsizer = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer.Add(imageMap, 0, wx.ALL, 5)
 
-class SetupTab(wx.Panel):
-    def __init__(self, parent):
-        super(SetupTab, self).__init__(parent=parent, id=wx.ID_ANY)
-        self.parent = parent
-        self.default_num_players = self.parent.get_default_num_players()
+        self.SetSizer(hsizer)
 
-        self.hsizer_input = wx.BoxSizer(wx.HORIZONTAL)
-        self.vsizer_input = wx.BoxSizer(wx.VERTICAL)
-        self.vsizer_overall = wx.BoxSizer(wx.VERTICAL)
+        self.Centre()
+        self.Show()
 
-        players_info_text = wx.StaticText(self, -1, "Number of players")
-        self.players_number = wx.TextCtrl(self, value=str(self.default_num_players))
-        btn_randomize = wx.Button(self, wx.ID_OK, label="Randomize setup", size=(140, 40))
-        self.Bind(wx.EVT_BUTTON, self.on_randomize, btn_randomize)
-
-        self.vsizer_input.Add(players_info_text, 1)
-        self.vsizer_input.Add(self.players_number, 1)
-
-        self.hsizer_input.Add(self.vsizer_input, 1, wx.EXPAND | wx.ALL, 20)
-        self.hsizer_input.Add(btn_randomize, 1, wx.EXPAND | wx.ALL, 20)
-        self.vsizer_overall.Add(self.hsizer_input, 0)    # setup
-
-        self.SetSizer(self.vsizer_overall)
-
-    def on_randomize(self, event):
-        n_players = int(self.players_number.GetValue())
-        random_setup = RandomSetup(self.parent, n_players)
-        random_setup.Show(True)
 
 class RandomSetup(wx.Frame):
     def __init__(self, parent, num_players):
         super(RandomSetup, self).__init__(parent, title="Random setup", size=(1200, 950))
-        self.parent = parent
-        self.num_players = num_players
-        self.resize_factor = 0.8
+        resize_factor = 0.8
 
-        self.hsizer_output = wx.BoxSizer(wx.HORIZONTAL)
-        self.vsizer_overall = wx.BoxSizer(wx.VERTICAL)
+        hsizer_output = wx.BoxSizer(wx.HORIZONTAL)
+        vsizer_overall = wx.BoxSizer(wx.VERTICAL)
 
         vsizer_output = wx.BoxSizer(wx.VERTICAL)
 
-        self.hsizer_tech_tracks = wx.BoxSizer(wx.HORIZONTAL)
-        self.hsizer_extra_tech = wx.BoxSizer(wx.HORIZONTAL)
-        self.hsizer_boosters = wx.BoxSizer(wx.HORIZONTAL)
-        self.hsizer_round_score = wx.BoxSizer(wx.HORIZONTAL)
-        self.vsizer_end_score = wx.BoxSizer(wx.VERTICAL)
+        hsizer_tech_tracks = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer_extra_tech = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer_boosters = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer_round_score = wx.BoxSizer(wx.HORIZONTAL)
+        vsizer_end_score = wx.BoxSizer(wx.VERTICAL)
 
-        self.htracks = [self.hsizer_tech_tracks, self.hsizer_extra_tech, self.hsizer_round_score, self.hsizer_boosters]
+        htracks = [hsizer_tech_tracks, hsizer_extra_tech, hsizer_round_score, hsizer_boosters]
 
         background = wx.Image(background_path, wx.BITMAP_TYPE_ANY)
         background_img = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap(background))
@@ -206,8 +142,8 @@ class RandomSetup(wx.Frame):
                 W = brown_fed.GetWidth()
                 H = brown_fed.GetHeight()
                 local_factor = 0.8
-                brown_fed = brown_fed.Scale(int(W * self.resize_factor * local_factor),
-                                            int(H * self.resize_factor * local_factor))
+                brown_fed = brown_fed.Scale(int(W * resize_factor * local_factor),
+                                            int(H * resize_factor * local_factor))
                 brown_fed_image = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap(brown_fed))
                 brown_vsizer1.Add(brown_fed_image, 1, wx.ALL)
 
@@ -215,13 +151,13 @@ class RandomSetup(wx.Frame):
                 img1 = wx.Image(path1, wx.BITMAP_TYPE_ANY)
                 W = img1.GetWidth()
                 H = img1.GetHeight()
-                img1 = img1.Scale(int(W * self.resize_factor), int(H * self.resize_factor))
+                img1 = img1.Scale(int(W * resize_factor), int(H * resize_factor))
                 img1_bm = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap(img1))
                 path2 = image_path + list_of_pieces[2][i] + image_format
                 img2 = wx.Image(path2, wx.BITMAP_TYPE_ANY)
                 W = img2.GetWidth()
                 H = img2.GetHeight()
-                img2 = img2.Scale(int(W * self.resize_factor), int(H * self.resize_factor))
+                img2 = img2.Scale(int(W * resize_factor), int(H * resize_factor))
                 img2_bm = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap(img2))
 
                 brown_vsizer2.Add(img1_bm, 1, wx.ALL, 5)
@@ -230,7 +166,7 @@ class RandomSetup(wx.Frame):
                 hsizer.Add(brown_vsizer1, 1, wx.ALL)
                 hsizer.Add(brown_vsizer2, 1, wx.ALL)
 
-                self.hsizer_tech_tracks.Add(hsizer, 1, wx.EXPAND | wx.ALL)
+                hsizer_tech_tracks.Add(hsizer, 1, wx.EXPAND | wx.ALL)
 
             else: # all other tracks
                 vsizer = wx.BoxSizer(wx.VERTICAL)
@@ -239,21 +175,21 @@ class RandomSetup(wx.Frame):
                 img1 = wx.Image(path1, wx.BITMAP_TYPE_ANY)
                 W = img1.GetWidth()
                 H = img1.GetHeight()
-                img1 = img1.Scale(int(W * self.resize_factor), int(H * self.resize_factor))
+                img1 = img1.Scale(int(W * resize_factor), int(H * resize_factor))
                 img1_bm = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap(img1))
                 path2 = image_path + list_of_pieces[2][i] + image_format
                 img2 = wx.Image(path2, wx.BITMAP_TYPE_ANY)
                 W = img2.GetWidth()
                 H = img2.GetHeight()
-                img2 = img2.Scale(int(W * self.resize_factor), int(H * self.resize_factor))
+                img2 = img2.Scale(int(W * resize_factor), int(H * resize_factor))
                 img2_bm = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap(img2))
 
                 vsizer.Add(img1_bm, 1, wx.ALL, 5)
                 vsizer.Add(img2_bm, 1, wx.ALL, 5)
 
-                self.hsizer_tech_tracks.Add(vsizer, 1, wx.EXPAND | wx.ALL)
+                hsizer_tech_tracks.Add(vsizer, 1, wx.EXPAND | wx.ALL)
 
-        vsizer_output.Add(self.hsizer_tech_tracks, 1, wx.EXPAND | wx.ALL, 5)
+        vsizer_output.Add(hsizer_tech_tracks, 1, wx.EXPAND | wx.ALL, 5)
 
         # Extra tech tiles
         extra_tech = list_of_pieces[2][6:9]
@@ -263,28 +199,27 @@ class RandomSetup(wx.Frame):
             img = wx.Image(path, wx.BITMAP_TYPE_ANY)
             W = img.GetWidth()
             H = img.GetHeight()
-            img = img.Scale(int(W * self.resize_factor), int(H * self.resize_factor))
+            img = img.Scale(int(W * resize_factor), int(H * resize_factor))
             img_bm = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap(img))
 
-            self.hsizer_extra_tech.Add(img_bm, 1, wx.ALL, 5)
+            hsizer_extra_tech.Add(img_bm, 1, wx.ALL, 5)
 
-        vsizer_output.Add(self.hsizer_extra_tech, 1, wx.EXPAND | wx.ALL, 5)
+        vsizer_output.Add(hsizer_extra_tech, 1, wx.EXPAND | wx.ALL, 5)
 
         # Boosters
-        n_players = self.num_players
-        num_boosters = n_players + 3
+        num_boosters = num_players + 3
 
         for i in range(num_boosters):
             path = image_path + list_of_pieces[4][i] + image_format
             img = wx.Image(path, wx.BITMAP_TYPE_ANY)
             W = img.GetWidth()
             H = img.GetHeight()
-            img = img.Scale(int(W * self.resize_factor), int(H * self.resize_factor))
+            img = img.Scale(int(W * resize_factor), int(H * resize_factor))
             img_bm = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap(img))
 
-            self.hsizer_boosters.Add(img_bm, 1, wx.ALL, 5)
+            hsizer_boosters.Add(img_bm, 1, wx.ALL, 5)
 
-        vsizer_output.Add(self.hsizer_boosters, 1, wx.EXPAND | wx.ALL, 5)
+        vsizer_output.Add(hsizer_boosters, 1, wx.EXPAND | wx.ALL, 5)
 
         # Round score
         for i in range(6):
@@ -292,10 +227,10 @@ class RandomSetup(wx.Frame):
             img = wx.Image(path, wx.BITMAP_TYPE_ANY)
             W = img.GetWidth()
             H = img.GetHeight()
-            img = img.Scale(int(W * self.resize_factor), int(H * self.resize_factor))
+            img = img.Scale(int(W * resize_factor), int(H * resize_factor))
             img_bm = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap(img))
 
-            self.hsizer_round_score.Add(img_bm, 1, wx.ALL, 5)
+            hsizer_round_score.Add(img_bm, 1, wx.ALL, 5)
 
         # End game score
         for i in range(2):
@@ -303,18 +238,18 @@ class RandomSetup(wx.Frame):
             img = wx.Image(path, wx.BITMAP_TYPE_ANY)
             W = img.GetWidth()
             H = img.GetHeight()
-            img = img.Scale(int(W * self.resize_factor), int(H * self.resize_factor))
+            img = img.Scale(int(W * resize_factor), int(H * resize_factor))
             img_bm = wx.StaticBitmap(self, wx.ID_ANY, wx.Bitmap(img))
 
-            self.vsizer_end_score.Add(img_bm, 1, wx.ALL, 5)
+            vsizer_end_score.Add(img_bm, 1, wx.ALL, 5)
 
-        self.hsizer_round_score.Add(self.vsizer_end_score, 1, wx.ALL, 5)
-        vsizer_output.Add(self.hsizer_round_score, 1, wx.EXPAND | wx.ALL, 5)
+        hsizer_round_score.Add(vsizer_end_score, 1, wx.ALL, 5)
+        vsizer_output.Add(hsizer_round_score, 1, wx.EXPAND | wx.ALL, 5)
 
-        self.hsizer_output.Add(vsizer_output, 1, wx.EXPAND | wx.ALL, 5)
-        self.vsizer_overall.Add(self.hsizer_output, 0, wx.EXPAND)
+        hsizer_output.Add(vsizer_output, 1, wx.EXPAND | wx.ALL, 5)
+        vsizer_overall.Add(hsizer_output, 0, wx.EXPAND)
 
-        self.SetSizer(self.vsizer_overall)
+        self.SetSizer(vsizer_overall)
 
         self.Centre()
         self.Show()
